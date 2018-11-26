b26528752d96e76ae7306567544a4d7bf375a476
Dmytro Heorhin <dmytro_heorhin@epam.com>
2018-11-26 12:54:30 +0200

    add mvc

---
+++ PlanningPoker/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace PlanningPoker
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-signalR").Include("~/Scripts/jquery.signalR-2.4.0.min.js"));
+        }
+    }
+}
---
+++ PlanningPoker/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace PlanningPoker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
